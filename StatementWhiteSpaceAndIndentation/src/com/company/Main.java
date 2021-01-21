2e126e9c205000ff5a238fabedd7ff34da73d9cc
madhavijaival <madhavijaival@users.noreply.github.com>
2021-01-20 17:23:23 -0800

    initial change

---
+++ StatementWhiteSpaceAndIndentation/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int myNumber = 50;
+        myNumber++;
+        myNumber--;
+
+        System.out.println("This is a test");
+        System.out.println("This is" +
+                " another " +
+                "still more.");
+        int myAnotherNumber = 50;
+        myNumber--;
+        System.out.println("This is another one");
+
+    }
+}
